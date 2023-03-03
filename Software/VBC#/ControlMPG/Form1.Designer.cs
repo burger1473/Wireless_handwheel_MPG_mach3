d664c949ac8805adb1003ccc2b0ffbb906d2da06
burger1473 <fabianburgos.09@gmail.com>
2023-03-02 21:46:48 -0300

    Subo avance con cliente tcp funcionando

--- Software/VBC#/ControlMPG/Form1.Designer.cs
+++ Software/VBC#/ControlMPG/Form1.Designer.cs
@@ -36,6 +36,9 @@ namespace ControlMPG
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Conexion_tcp_automatica = new System.Windows.Forms.Timer(this.components);
+            this.timer_tcpvivo = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // timer1
@@ -86,11 +89,34 @@ namespace ControlMPG
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start server tcp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Conexion_tcp_automatica
+            // 
+            this.Conexion_tcp_automatica.Enabled = true;
+            this.Conexion_tcp_automatica.Interval = 500;
+            this.Conexion_tcp_automatica.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer_tcpvivo
+            // 
+            this.timer_tcpvivo.Enabled = true;
+            this.timer_tcpvivo.Interval = 1500;
+            this.timer_tcpvivo.Tick += new System.EventHandler(this.timer_tcpvivo_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox2);
@@ -98,6 +124,7 @@ namespace ControlMPG
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -112,6 +139,9 @@ namespace ControlMPG
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer Conexion_tcp_automatica;
+        private System.Windows.Forms.Timer timer_tcpvivo;
     }
 }
 
--- Software/VBC#/ControlMPG/Form1.cs
+++ Software/VBC#/ControlMPG/Form1.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;   //proporciona servicios de interoperabilidad para la comunicación entre componentes de software escritos en lenguajes de programación diferentes. Esto significa que puede usar el espacio de nombres para comunicarse con componentes de software escritos en lenguajes como C#, Visual Basic y C++. Esto se logra mediante la creación de una interfaz común entre los componentes de software, lo que permite que los componentes de software escritos en diferentes lenguajes de programación se comuniquen entre sí.
 using Mach4;                            //DLL Interop.Mach4.dll  creado al referir el icono de Mach3. Antes se debia crear el registro.
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 
 namespace ControlMPG
 {
@@ -17,6 +21,13 @@ namespace ControlMPG
         //Defgino variable connect Mach3
         private IMach4 _mach;
         private IMyScriptObject _mInst;
+        //string ip = "192.168.4.1";
+        //string ip = "192.168.1.113";
+        string ip;
+        int puerto_server1 = 8001;
+        private Socket clientSocket;
+        private byte[] buffer;
+        bool tcp_conected = false;
 
         public Form1()
         {
@@ -58,12 +69,23 @@ namespace ControlMPG
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            IPAddress[] IpInHostAddress = Dns.GetHostAddresses("MPG000001.local");
+            ip = IpInHostAddress[0].ToString();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             UpdateDro();
+            textBox1.Text = tcp_conected.ToString();
+            richTextBox1.Text = Conexion_tcp_automatica.Enabled.ToString();
+            try
+            {
+                if (clientSocket.Connected==false) {
+                    tcp_conected=false;
+                }
+            }
+            catch (Exception) { 
+            }
         }
 
         private void Load_Gcode_Click(object sender, EventArgs e)
@@ -76,11 +98,11 @@ namespace ControlMPG
                 //_mInst.DoOEMButton(307); //Genera un pulso en eje x ++
                 //_mInst.DoOEMButton(308); //Genera un pulso en eje x --
                 //_mInst.DoOEMButton(309); //Genera un pulso en eje y ++
-                //_mInst.DoOEMButton(310); //Genera un pulso en eje y ++
+                //_mInst.DoOEMButton(310); //Genera un pulso en eje y --
                 //_mInst.DoOEMButton(311); //Genera un pulso en eje z ++
-                //_mInst.DoOEMButton(312); //Genera un pulso en eje z ++
+                //_mInst.DoOEMButton(312); //Genera un pulso en eje z --
                 //_mInst.DoOEMButton(313); //Genera un pulso en eje a ++
-                //_mInst.DoOEMButton(314); //Genera un pulso en eje a ++
+                //_mInst.DoOEMButton(314); //Genera un pulso en eje a --
                 //_mInst.DoOEMButton(1007); //Zero all
                 //_mInst.DoOEMButton(1008); //Zero x
                 //_mInst.DoOEMButton(1009); //Zero y
@@ -106,7 +128,186 @@ namespace ControlMPG
 
         private void button1_Click(object sender, EventArgs e)
         {
+            enviar_tcp("HOLAfabi");
+        }
 
+    
+        void desconectar()
+        {
+            try
+            {
+                if (clientSocket.Connected == true)
+                {
+                    tcp_conected = false;
+                    Conexion_tcp_automatica.Enabled = true;
+                    //clientSocket.Disconnect(true);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void enviar_tcp(string mensaje)
+        {
+            byte[] enviar_info = new byte[100];
+            enviar_info = Encoding.Default.GetBytes(mensaje);
+            try
+            {
+                clientSocket.BeginSend(enviar_info, 0, enviar_info.Length, SocketFlags.None, SendCallback, null);
+            }
+            catch (SocketException ex)
+            {
+
+            }
+            catch (ObjectDisposedException ex)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private static void ShowErrorDialog(string message)
+        {
+            MessageBox.Show(message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                int received = clientSocket.EndReceive(AR);
+
+                string message = Encoding.ASCII.GetString(buffer);
+                textBox1.Text = message;
+                richTextBox1.Text = richTextBox1.Text + "/n" + message;
+
+                // Comienzo a recibir datos nuevamente.
+                clientSocket.BeginReceive(buffer, 0, 2, SocketFlags.None, ReceiveCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                desconectar();
+            }
+            catch (ObjectDisposedException ex)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+
+        private void ConnectCallback(IAsyncResult AR)
+        {
+            try
+            {
+                //clientSocket = (Socket)AR.AsyncState;
+                clientSocket.EndConnect(AR);
+                buffer = new byte[clientSocket.ReceiveBufferSize];
+                clientSocket.BeginReceive(buffer, 0, 2, SocketFlags.None, ReceiveCallback, null);
+
+            }
+            catch (SocketException ex)
+            {
+                desconectar();
+            }
+            catch (ObjectDisposedException ex)
+            {
+            
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SendCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket.EndSend(AR);
+            }
+            catch (SocketException ex)
+            {  
+                desconectar();
+            }
+            catch (ObjectDisposedException ex)
+            {
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           if (tcp_conected == true) {
+                try
+                {
+                        clientSocket.Disconnect(true);
+                        clientSocket.Close();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (tcp_conected == false)
+            {
+                try
+                {
+                    
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                   
+                    // Connect to the specified host.
+                    if (clientSocket.Connected == false)
+                    {
+                        var endPoint = new IPEndPoint(IPAddress.Parse(ip), puerto_server1);
+                        clientSocket.BeginConnect(endPoint, new AsyncCallback(ConnectCallback), null);
+                        tcp_conected = true;
+                        byte[] enviar_info = new byte[100];
+                        enviar_info = Encoding.Default.GetBytes("N04Conected*****************************************F");
+                        clientSocket.BeginSend(enviar_info, 0, enviar_info.Length, SocketFlags.None, SendCallback, null);
+                        //Conexion_tcp_automatica.Enabled = false;
+                    }
+                }
+                catch (SocketException ex)
+                {
+
+                }
+                catch (ObjectDisposedException ex)
+                {
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void timer_tcpvivo_Tick(object sender, EventArgs e)
+        {
+            enviar_tcp("N05Vivo0000000**************************************F");
         }
     }
 }
